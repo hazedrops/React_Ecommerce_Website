5ca0a498fb0338df2ecac2e299e00443c7d72a66
hazedrops <hazedrops@hotmail.com>
2024-05-08 21:45:42 -0700

    Popular, Item, Hero components are added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
-import Navbar from "./Components/Navbar/Navbar"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Shop } from "./Pages/Shop"
-import { ShopCategory } from "./Pages/ShopCategory"
-import { Product } from "./Pages/Product"
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Shop } from './Pages/Shop'
+import { ShopCategory } from './Pages/ShopCategory'
+import { Product } from './Pages/Product'
+import { Cart } from './Pages/Cart'
+import { LoginSignup } from './Pages/LoginSignup'
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path='/product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </div>
